33ab592670aaca1f2775e71fe0ee6c82b94ad54a
song940 <song940@gmail.com>
2016-01-28 17:39:17 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,106 @@
+/**
+ * [openApp description]
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ *
+ *
+ * TEST CASE:
+ *
+ * location.href : {
+ *   ios8: {
+ *     INSTALLED: OK
+ *     NOT INSTALLED: ALERT ERROR
+ *   }
+ *   ios9: {
+ *    INSTALLED: OK
+ *    NOT INSTALLED: ALERT ERROR
+ *   }
+ *   android: {
+ *     INSTALLED: OK
+ *     NOT INSTALLED: REDIRECT TO ERROR PAGE
+ *   }
+ * }
+ *
+ * iframe: {
+ *   ios8: {
+ *     INSTALLED: ok
+ *     NOT INSTALLED: nothing
+ *   }
+ *   ios9: {
+ *     INSTALLED: nothing
+ *     NOT INSTALLED: nothing
+ *   }
+ *   android: {
+ *
+ *   }
+ * }
+ *
+ */
+function openApp(options){
+  var ua = navigator.userAgent;
+  var isChrome  = /chrome/i.test(ua);
+  var isSamsung = /samsung/i.test(ua);
+  var os        = (/iphone|android/i.exec(ua) || [])[0].toLowerCase();
+  var version   = parseInt((/version\/(\d\.\d)/i.exec(ua) || [])[1], 10);
+  /**
+   * [createIFrame description]
+   * @param  {[type]} src [description]
+   * @return {[type]}     [description]
+   */
+  function createIFrame(src){
+    var iframe = document.createElement('iframe');
+    iframe.src = src;
+    document.body.appendChild(iframe);
+    document.body.removeChild(iframe);
+  }
+  /**
+   * Android Intents with Chrome
+   * see docs https://developer.chrome.com/multidevice/android/intents
+   *
+   * intent://
+   *  scan/
+   *  #Intent;
+   *   package=com.google.zxing.client.android;
+   *   schema=meituanmovie;
+   * end;
+   */
+  function buildIntent(url){
+    var scheme, action = url.replace(/^(\w+):\/\//, function(_, m){
+      scheme = m;
+      return '';
+    });
+    //
+    var o = {
+     scheme  : scheme,
+     package : options.package
+    };
+    //
+    var meta = Object.keys(o).map(function(key){
+      return [ key, o[ key ] ].join('=');
+    }).map(function(part){
+      return part + ';';
+    }).join('');
+    //
+    return 'intent://' + action + '#Intent;' + meta + 'end;'
+  };
+  /**
+   * [open description]
+   * @return {[type]} [description]
+   */
+  return function open(){
+    /**
+     * android support iframe call schema (NOT include SAMSUNG devices),
+     * iOS9 and later is no longer support iframe call schema.
+     */
+    if(os == 'android' || (os == 'iphone' && version < 9)){
+      if(isSamsung){
+        createIFrame(buildIntent(options.schema));
+      }else{
+        createIFrame(options.schema);
+      }
+    }else{
+      // iOS8 and earlier .
+      location.href = options.schema;
+    }
+  };
+};
